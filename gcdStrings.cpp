46f8132fa01ea55759ecefdb679623618f428a87
HarshVardhanGeu <79054610+HarshVardhanGeu@users.noreply.github.com>
2022-01-03 19:21:56 +0530

    Q6

---
+++ gcdStrings.cpp
@@ -0,0 +1,16 @@
+ int gcd(int a,int b)
+    {
+        if(b==0) return a;
+        
+        return gcd(b,a%b);
+    }
+    string gcdOfStrings(string str1, string str2) {
+        
+        if(str1+str2!=str2+str1)
+             return "";
+        
+        int len_gcd=gcd(str1.length(),str2.length());
+        
+        return str1.substr(0,len_gcd);
+        
+    }
